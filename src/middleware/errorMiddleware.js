e146025d38933a73e9b6e7138ce47f4321627782
Mulky Ayyubi <170484966+MulkyAyyubi@users.noreply.github.com>
2025-10-24 11:13:33 +0700

    feat: fix a small error

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,5 +1,5 @@
 export const errorMiddleware = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
+    const statusCode = err.status || 500;
     const bodyStatus = statusCode <= 500 ? "fail" : "error";
 
     res.status(statusCode).json({
